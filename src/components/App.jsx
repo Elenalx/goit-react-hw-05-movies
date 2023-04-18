3c1ba8353a5cfe457582c0517796b5bab62b3734
Elena Blagodir <elenalx28@gmail.com>
2023-04-18 22:47:44 +0300

    add1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,23 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+// import { useState } from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+import Home from './Pages/Home/Home';
+import Movies from './Pages/Movies/Movies';
+import MoviesDetails from './Pages/MovieDetails/MoviesDetails';
+import Cast from './Pages/Cast/Cast';
+import Reviews from './Pages/Reviews/Reviews';
+
+
+const App = () => {
+  return <Routes>
+    <Route path='/' element={<Home />} />
+    <Route path='/' element={<Movies />} />
+    <Route path='/' element={<MoviesDetails />} />
+    <Route path='/' element={<Cast />} />
+    <Route path='/' element={<Reviews />} />
+
+  </Routes>
+    
 };
+
+export default App

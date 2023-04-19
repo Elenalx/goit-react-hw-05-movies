cc32e5a20a6fadfb0dbcf08d5350856ac1fcc38b
Elena Blagodir <elenalx28@gmail.com>
2023-04-19 11:31:58 +0300

    add2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,24 @@
 // import { useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
 
-import Home from './Pages/Home/Home';
-import Movies from './Pages/Movies/Movies';
-import MoviesDetails from './Pages/MovieDetails/MoviesDetails';
-import Cast from './Pages/Cast/Cast';
-import Reviews from './Pages/Reviews/Reviews';
+import Home from './pages/Home/Home';
+import Movies from './pages/Movies/Movies';
+import MoviesDetails from './pages/MovieDetails/MoviesDetails';
+import Cast from './pages/Cast/Cast';
+import Reviews from './pages/Reviews/Reviews';
 
 
 const App = () => {
-  return <Routes>
-    <Route path='/' element={<Home />} />
-    <Route path='/' element={<Movies />} />
-    <Route path='/' element={<MoviesDetails />} />
-    <Route path='/' element={<Cast />} />
-    <Route path='/' element={<Reviews />} />
-
-  </Routes>
+  return (
+     <Routes>
+         <Route path='/' element={<Home />} />
+         <Route path='/' element={<Movies />} />
+         <Route path='/' element={<MoviesDetails />} />
+         <Route path='/' element={<Cast />} />
+         <Route path='/' element={<Reviews />} />
+    </Routes>
+    
+  )
     
 };
 
--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import BrowserRouter from 'react';
+import { BrowserRouter } from 'react';
 import  App  from 'components/App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-);
+ <React.StrictMode>
+    <BrowserRouter basename="goit-react-hw-05-movies">
+      <App/>
+    </BrowserRouter>
+  </React.StrictMode>
+)

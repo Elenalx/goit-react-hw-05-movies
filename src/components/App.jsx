5eaa0c10f3dea7d7139dc156488d196d1ff06c5a
Elena Blagodir <elenalx28@gmail.com>
2023-04-20 16:22:44 +0300

    add4

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,24 @@
 // import { useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
 
-import Home from './pages/Home/Home';
-import Movies from './pages/Movies/Movies';
-import MoviesDetails from './pages/MovieDetails/MoviesDetails';
-import Cast from './pages/Cast/Cast';
-import Reviews from './pages/Reviews/Reviews';
+import Home from 'pages/Home/Home';
+import Movies from 'pages/Movies/Movies';
+import MoviesDetails from 'pages/MovieDetails/MoviesDetails';
+import Cast from 'pages/Cast/Cast';
+import Reviews from 'pages/Reviews/Reviews';
 
 
 const App = () => {
   return (
-     <Routes>
+    <>
+      <Routes>
          <Route path='/' element={<Home />} />
          <Route path='/movies' element={<Movies />} />
          <Route path='/movies/:movieId' element={<MoviesDetails />} />
-         <Route path='/:movieId/cast' element={<Cast />} />
-         <Route path='/:movieId/reviews' element={<Reviews />} />
-    </Routes>
+         <Route path='cast' element={<Cast />} />
+         <Route path='reviews' element={<Reviews />} />
+      </Routes>
+    </>
     
   )
     
---
+++ src/services/getMovies.js
@@ -0,0 +1,3 @@
+const API_KEY = "d31c9faeabd85b83726848cf0b50c5a1"
+const BASE_URL = "https://api.themoviedb.org/3/"
+
